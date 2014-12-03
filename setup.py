ca21adb28af1d73e5e81996f300711f0c33b12ae
Eric Hulser <ehulser@teslamotors.com>
2014-12-03 15:10:18 -0800

    upresed for release with the new init_extension function name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'mkdocs_autodoc',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'Eric Hulser',
     author_email = 'eric.hulser@projexsoftware.com',
     maintainer = 'Eric Hulser',
